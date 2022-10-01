267028201156d4d7752df62419401a65b217e19a
Melissa <mel.chavez.code@gmail.com>
2022-09-30 22:08:14 -0400

    add cache for site

--- sw_cached_site.js
+++ sw_cached_site.js
@@ -1,19 +1,10 @@
-const cacheName = "v1";
-const cacheAssets = ["index.html", "about.html", "js/script.js"];
+//caching the whole site
+const cacheName = "v2";
 
 //life cycle of service worker: register, install, activate
 //call install event
 self.addEventListener("install", (e) => {
   console.log("Service Worker: Installed");
-  e.waitUntil(
-    caches
-      .open(cacheName)
-      .then((cache) => {
-        console.log("Service Worker: Caching Files");
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting())
-  );
 });
 
 //call activate event
@@ -39,5 +30,19 @@ self.addEventListener("activate", (e) => {
 self.addEventListener("fetch", (e) => {
   console.log("Service Worker: Fetching");
   //check if online, if offline err, then use cached files
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        //make copy/clone of response
+        const resClone = res.clone();
+        //open cache
+        caches.open(cacheName).then((cache) => {
+          //add response to cache
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch((err) => caches.match(e.request))
+      .then((res) => res)
+  );
 });
